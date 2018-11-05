badf17a9c56e706e2ee165b799202a792f799c11
unknown <nitish.ghimire1@gmail.com>
2018-11-05 17:33:03 +0545

    solved the money format error

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="with-background">
+    <div class="footer-content container">
+        <div class="made-with">Made with <i class="fa fa-heart heart"></i> by KUSH TECHNOLOGIES</div>
+        {{ menu('footer', 'partials.menus.footer') }}
+    </div> <!-- end footer-content -->
+</footer>
